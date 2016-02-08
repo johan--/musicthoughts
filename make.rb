8316fcf3162896d0c43a81fbb36f4a227935520d
Derek Sivers <derek@sivers.org>
2016-02-09 12:18:24 +1300

    mkdir + random + approved thoughts in json

--- make.rb
+++ make.rb
@@ -3,3 +3,22 @@ db = getdb('musicthoughts')
 
 # langs = array of 2-char strings: en, es, fr, etc.
 ok, langs = db.call('languages')
+
+langs.each do |lang|
+	site = 'site/%s/' % lang
+	# make subdirectories, if they don't exist
+	%w(t author contributor).each do |subdir|
+		Dir.mkdir(site + subdir) unless File.directory?(site + subdir)
+	end
+
+	# get all approved thoughts & random thoughts.  write json
+	ok, approved_thoughts = db.call('approved_thoughts', lang)
+	File.open(site + 'thoughts.json', 'w') {|f| f.puts approved_thoughts.to_json }
+	ok, random_thoughts = db.call('random_thoughts', lang)
+	File.open(site + 'random.json', 'w') {|f| f.puts random_thoughts.to_json }
+
+	# write home
+	# write approved thought pages
+	# write author pages
+	# write contributor pages
+end
